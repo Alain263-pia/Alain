dd4332cd9a494e37bd86da395ee37c40a0cc3ea1
hehe <huyheo08@gmail.com>
2026-03-09 08:59:18 -0700

    broo

--- snowball.py
+++ snowball.py
@@ -1,2 +1,23 @@
 print ("helo from snowball")
 print ("a print from pycharm")
+
+
+
+#import sys
+#sys.stdin = open('in.txt','r')
+#sys.stdout= open('out.txt','w')
+
+turn = 1
+stones = 7
+running = True
+while True:
+    print("#stones:",stones,"     player",turn)
+    choice = int(input("enter your choice: "))
+    if choice==1:
+        stones -=1
+    if choice==2:
+        stones -=2
+    if stones<=0:
+        print("player",turn,"win the game")
+        running = False
+    turn = 3- turn
